05042022af35c9266e0dc92b8b6f15391608851e
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2024-04-10 14:21:00 +0100

    remove redundant index

--- lua/arrow/buffer_ui.lua
+++ lua/arrow/buffer_ui.lua
@@ -73,7 +73,7 @@ function M.spawn_preview_window(buffer, index, bookmark, bookmark_count)
 		border = "single",
 	}
 
-	local displayIndex = config.getState("index_keys"):sub(index, index)
+	-- local displayIndex = config.getState("index_keys"):sub(index, index)
 
 	local win = vim.api.nvim_open_win(buffer, true, window_config)
 
@@ -85,7 +85,7 @@ function M.spawn_preview_window(buffer, index, bookmark, bookmark_count)
 
 	vim.api.nvim_win_set_option(win, "scrolloff", 999)
 	vim.api.nvim_win_set_cursor(win, { bookmark.line, 0 })
-	vim.api.nvim_win_set_config(win, { title = displayIndex .. " " .. extra_title })
+	vim.api.nvim_win_set_config(win, { title = "" .. extra_title })
 	vim.api.nvim_win_set_option(win, "number", true)
 
 	table.insert(preview_buffers, { buffer = buffer, win = win, index = index })
