06d2362a67801918c7edd9e9cc7dd53190aad50e
xzbdmw <97848247+xzbdmw@users.noreply.github.com>
2024-04-11 05:38:03 +0800

    Update buffer_ui.lua

--- lua/arrow/buffer_ui.lua
+++ lua/arrow/buffer_ui.lua
@@ -92,12 +92,11 @@ function M.spawn_preview_window(buffer, index, bookmark, bookmark_count)
 	vim.api.nvim_win_set_config(win, { title = displayIndex .. " " .. extra_title })
 	vim.api.nvim_win_set_option(win, "number", true)
 	
-	if config.getState("per_buffer_config").treesitter_context ~= nil then
-		local ctx_config = config.getState("per_buffer_config").treesitter_context
+	local ctx_config = config.getState("per_buffer_config").treesitter_context
+	if ctx_config ~= nil and ctx_config.line_shift_down ~= nil then
 		local shift = ctx_config.line_shift_down
 
 		local win_view = vim.fn.winsaveview()
-
 		vim.api.nvim_win_set_option(win, "scrolloff", 0)
 		vim.fn.winrestview({ topline = win_view.topline - shift })
 
@@ -105,6 +104,7 @@ function M.spawn_preview_window(buffer, index, bookmark, bookmark_count)
 		if not ok then
 			vim.notify("you don't have treesitter-context installed", vim.log.levels.WARN)
 		end
+
 		local context, context_lines = require("treesitter-context.context").get(buffer, win)
 		if context and #context > 0 then
 			vim.defer_fn(function()
